b517c6dbca2285770491614a695441d05c231223
nadhem <nadhem@nadhem-PC>
2018-04-24 20:40:44 +0200

    .

--- kpiGui/src/example1/JavaFXFileBrowseDemoApp.java
+++ kpiGui/src/example1/JavaFXFileBrowseDemoApp.java
@@ -6,20 +6,21 @@ import java.nio.file.Path;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFXFileBrowseDemoApp extends Application {
-	private TreeView<Object> treeView;
-
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -27,41 +28,28 @@ public class JavaFXFileBrowseDemoApp extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		// create tree pane
-		VBox treeBox = new VBox();
-		treeBox.setPadding(new Insets(10, 10, 10, 10));
-		treeBox.setSpacing(10);
-		// setup the file browser root
-		String hostName = "computer";
-		try {
-			hostName = InetAddress.getLocalHost().getHostName();
-		} catch (Exception x) {
-		}
-		ImageView rooticon = new ImageView(new Image("file:resources/images/IBM_Notes.png"));
-		rooticon.setFitHeight(30);
-		rooticon.setFitWidth(30);
-		TreeItem<Object> rootNode = new TreeItem<>(hostName, rooticon);
+		HBox hBox0 = new HBox();
+		hBox0.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
+				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
+
+		Button button1 = new Button("button1");
+		Button button2 = new Button("button2");
+		hBox0.setAlignment(Pos.CENTER);
+		HBox hBox1 = new HBox(button1);
+		hBox1.setAlignment(Pos.CENTER_LEFT);
+		HBox.setHgrow(hBox1,Priority.SOMETIMES);
+//		hBox1.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
+//				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
+		HBox hBox2 = new HBox(button2);
+		hBox2.setAlignment(Pos.CENTER_RIGHT);
+		HBox.setHgrow(hBox2,Priority.SOMETIMES);
 		
-		for (int i = 0; i < 5; i++) {
-			ImageView nodeIcon = new ImageView(new Image("file:resources/images/view.png"));
-			nodeIcon.setFitHeight(30);
-			nodeIcon.setFitWidth(30);
-			TreeItem<Object> node = new TreeItem<>(hostName, nodeIcon);
-			node.setExpanded(true);
-			rootNode.getChildren().add(node);
-		}
+//		hBox2.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
+//				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
 
-		rootNode.setExpanded(true);
-		// create the tree view
-		treeView = new TreeView<>(rootNode);
-		// add everything to the tree pane
-		treeBox.getChildren().addAll(new Label("File browser"), treeView);
-		VBox.setVgrow(treeView, Priority.ALWAYS);
+		hBox0.getChildren().addAll(hBox1,hBox2);
 
-		// setup and show the window
-		primaryStage.setTitle("JavaFX File Browse Demo");
-		StackPane root = new StackPane();
-		root.getChildren().addAll(treeBox);
-		primaryStage.setScene(new Scene(root, 400, 300));
+		primaryStage.setScene(new Scene(hBox0, 400, 300));
 		primaryStage.show();
 	}
 }
