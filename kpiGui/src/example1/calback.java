2575d519d5dbb05c07698f0a34ad9d08df93bdc9
nadhem <nadhem.jemmali@ensi-uma.tn>
2018-04-27 17:52:43 +0100

    Create calback.java

---
+++ kpiGui/src/example1/calback.java
@@ -0,0 +1,34 @@
+package example3;
+
+import java.util.function.Function;
+
+public class ThreadListner {
+
+	public ThreadListner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		FirstClass first = new FirstClass("first");
+		SecondClass second = new SecondClass();
+		System.out.println(second.applyFunction("second", first::addPrefix));
+	}
+}
+
+class FirstClass {
+	String prefix;
+
+	public FirstClass(String prefix) {
+		this.prefix = prefix;
+	}
+
+	public String addPrefix(String suffix) {
+		return prefix + ":" + suffix;
+	}
+}
+
+class SecondClass {
+	public String applyFunction(String name, Function<String, String> function) {
+		return function.apply(name);
+	}
+}
